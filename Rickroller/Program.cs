13f84ead66636b0785aaf8489a114a8890b91307
Guillermo Castro Garcia <Guillermo.CastroGarcia@digitecgalaxus.ch>
2016-08-18 16:26:31 +0200

    This is gonna be a hit

---
+++ Rickroller/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Process.Start(@"https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+                Console.WriteLine("Congrats, you've been rickrolled");
+            }
+            catch
+            {
+                Console.WriteLine("Something went wrong. Probably you're not goot enough to be rickanrolled");
+            }
+        }
+    }
+}
