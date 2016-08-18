f89a6438adbe30810c7fedb63e855acf00b58e34
Guillermo Castro Garcia <Guillermo.CastroGarcia@digitecgalaxus.ch>
2016-08-18 16:37:47 +0200

    Converted into class library

--- Rickroller/Program.cs
+++ Rickroller/Program.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApplication2
+namespace Rickroller
 {
-    class Program
+    public static class Rickroller
     {
-        static void Main(string[] args)
+        public static void RickrollMe()
         {
             try
             {
