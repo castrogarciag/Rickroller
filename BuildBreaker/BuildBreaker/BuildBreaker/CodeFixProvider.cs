d021be52875f79864113d2b836f19acb7e2411b8
Guillermo Castro Garcia <Guillermo.CastroGarcia@digitecgalaxus.ch>
2016-08-19 09:18:12 +0200

    Added buildBreaker Analyzer project

---
+++ BuildBreaker/BuildBreaker/BuildBreaker/CodeFixProvider.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Rename;
+using Microsoft.CodeAnalysis.Text;
+
+namespace BuildBreaker
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(BuildBreakerCodeFixProvider)), Shared]
+    public class BuildBreakerCodeFixProvider : CodeFixProvider
+    {
+        private const string title = "Won't fix anything";
+
+        public sealed override ImmutableArray<string> FixableDiagnosticIds
+        {
+            get { return ImmutableArray.Create(BuildBreakerAnalyzer.DiagnosticId); }
+        }
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+            // TODO: Replace the following code with your own analysis, generating a CodeAction for each fix to suggest
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            // Find the type declaration identified by the diagnostic.
+            var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<TypeDeclarationSyntax>().First();
+
+            // Register a code action that will invoke the fix.
+            context.RegisterCodeFix(
+                CodeAction.Create(
+                    title: title,
+                    createChangedSolution: c => MakeUppercaseAsync(context.Document, declaration, c),
+                    equivalenceKey: title),
+                diagnostic);
+        }
+
+        private async Task<Solution> MakeUppercaseAsync(Document document, TypeDeclarationSyntax typeDecl, CancellationToken cancellationToken)
+        {
+            return document.Project.Solution;
+        }
+    }
+}
---
+++ BuildBreaker/BuildBreaker/BuildBreaker/DiagnosticAnalyzer.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace BuildBreaker
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class BuildBreakerAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "BuildBreaker";
+
+        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+        // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
+        private static readonly LocalizableString Title = "SPAM!!";
+        private static readonly LocalizableString MessageFormat = "SPAM SPAM SPAM!!";
+        private static readonly LocalizableString Description = "SPAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAM!";
+        private const string Category = "Nosense";
+
+        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeNode, Enum.GetValues(typeof(SyntaxKind)).Cast<SyntaxKind>().ToArray());
+        }
+
+        private static void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
+            // For all such symbols, produce a diagnostic.
+            var diagnostic = Diagnostic.Create(Rule, context.Node.GetLocation(), context.Node.ToString());
+
+            context.ReportDiagnostic(diagnostic);
+        }
+    }
+}
